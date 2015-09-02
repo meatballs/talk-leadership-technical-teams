180d771023feac7575779524d82e07277a529bbf
Owen Campbell <owen.campbell@empiria.co.uk>
2015-09-02 20:00:36 +0100

    tidies up style plot

--- style_plot.py
+++ style_plot.py
@@ -2,19 +2,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 with plt.xkcd():
-    # Based on "Stove Ownership" from XKCD by Randall Monroe
-    # http://xkcd.com/418/
+
+    x = np.linspace(-10, 10, 100, endpoint=True)
+    y = x
 
     fig = plt.figure()
-    ax = fig.add_axes((0.1, 0.2, 0.8, 0.7))
-    ax.spines['right'].set_color('none')
-    ax.spines['top'].set_color('none')
+    plt.plot(x, y)
+
     plt.xticks([])
     plt.yticks([])
+    plt.xlabel('Relative Expertise ->')
+    plt.ylabel('Authoritarianism ->')
 
-    plt.plot([1,2,3,4])
-
-    plt.xlabel('Your Relative Expertise ->')
-    plt.ylabel('Appropriate Authoritarianism ->')
+    ax = plt.gca()
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.spines['bottom'].set_position(('data',0))
+    ax.spines['left'].set_position(('data',0))
 
     fig.savefig('images/style.png')
