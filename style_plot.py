6affdd309591dad4cb80ef7be4628ac0e882930b
Owen Campbell <owen.campbell@empiria.co.uk>
2015-09-04 15:01:52 +0100

    increases text size in plot

--- style_plot.py
+++ style_plot.py
@@ -6,7 +6,7 @@ with plt.xkcd():
     x = np.linspace(-10, 10, 100, endpoint=True)
     y = x
 
-    fig = plt.figure()
+    fig = plt.figure(figsize=(4, 4))
     plt.plot(x, y)
 
     plt.xticks([-10, 10], ['Rookie', 'Guru'])
@@ -15,7 +15,7 @@ with plt.xkcd():
     plt.ylabel('Style')
 
     axes = plt.gca()
-    axes.xaxis.set_label_coords(0.65, 0.45)
+    axes.xaxis.set_label_coords(0.7, 0.45)
     axes.yaxis.set_label_coords(0.45, 0.65)
     for spine in axes.spines.itervalues():
         spine.set_position(('data', 0))
